83b797bb9860313fe3e438ccdfe6c9a544266a15
ZazzyDictionary <zac.hong01@gmail.com>
2020-11-28 21:31:27 +0800

    :sparkles: week6

--- Telegramme/Telegramme/Controller/FriendController.swift
+++ Telegramme/Telegramme/Controller/FriendController.swift
@@ -18,9 +18,9 @@ class FriendController {
         
         let friendEntity = NSEntityDescription.entity(forEntityName:"CDFriend", in:context)!
         
-        let friend = NSManagedObject(entity:friendEntity, insertInto: context)
-        friend.setValue(friend.name , forKeyPath:"name")
-        friend.setValue(friend.profileImageName , forKeyPath:"profileImageName")
+        let friendObj = NSManagedObject(entity:friendEntity, insertInto: context)
+        friendObj.setValue(friend.name , forKeyPath:"name")
+        friendObj.setValue(friend.profileImageName , forKeyPath:"profileImageName")
         
         
         do {
@@ -30,19 +30,18 @@ class FriendController {
         }
     }
     
-    func AddMessageToFriend(friend:Friend, message:Message) {
+    func AddMessageToFriend(friend : Friend, message : Message) {
         let appDelegate = (UIApplication.shared.delegate) as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
-        
         let messageEntity = NSEntityDescription.entity(forEntityName:"CDMessage", in:context)!
-        
-        let message = NSManagedObject(entity:messageEntity, insertInto: context)
-        message.setValue(message.isSender , forKeyPath:"isSender")
-        message.setValue(message.text , forKeyPath:"text")
-        message.setValue(message.date , forKeyPath:"date")
-        message.setValue(friend , forKeyPath:"friend")
-        
+            
         do {
+            let messageObject = NSManagedObject(entity:messageEntity, insertInto: context)
+            messageObject.setValue(message.isSender , forKeyPath:"isSender")
+            messageObject.setValue(message.text , forKeyPath:"text")
+            messageObject.setValue(message.date , forKeyPath:"date")
+            messageObject.setValue(friend , forKeyPath:"friend")
+            
             try context.save()
         } catch let error as NSError {
             print("Could not save. \(error), \(error.userInfo)")
@@ -52,6 +51,25 @@ class FriendController {
     func retriveMessagesByFriend(friend:Friend) -> [Message]{
         let appDelegate = (UIApplication.shared.delegate) as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "CDFriend")
+        fetchRequest.predicate = NSPredicate(format: "name = %@", friend.name)
+        
+        var messages_list : [Message] = []
+        do {
+            let query = try context.fetch(fetchRequest)
+            let friendObj = query[0] as! NSManagedObject
+            
+            let messages = friendObj.value(forKey: "message")
+            for m in messages as! [NSManagedObject]{
+                let message:Message = Message(text: m.value(forKey: "text") as! String, isSender: m.value(forKey: "isSender") as! Bool, date: m.value(forKey: "date") as! Date)
+                messages_list.append(message)
+            }
+            return messages_list
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+            return []
+        }
         
         
     }
